96850632e55db51900760efb85f249cef37e78d0
Jordan Abi Nader <jordanabinader@Jordans-MacBook-Pro.local>
2023-12-04 14:41:10 -0500

    Wrong inputs

--- client/src/components/TestConstants.js
+++ client/src/components/TestConstants.js
@@ -10,8 +10,9 @@ import Loader from './Loader'
 const TestConstants = () => {
 
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [isCompliant, setIsCompliant] = useState(true)
   const [testSetup, setTestSetup] = useState({
     testName: '',
     material: '',
@@ -37,9 +38,6 @@ const TestConstants = () => {
     let errorMessageIn = '';
 
     try {
-
-      setIsLoading(true);
-
       let dataToSend;
       if (testSetup instanceof FormData) {
         dataToSend = Object.fromEntries(testSetup.entries());
@@ -50,16 +48,19 @@ const TestConstants = () => {
       const response = await axios.post('http://localhost:2999/startTest', testSetup);
       console.log('Test started successfully:', response.data);
       testId = response.data.testId; 
+      setIsCompliant(true);
       } catch (error) {
         console.error('Error starting test:', error);
-        errorMessageIn = 'Error starting test: ' + error.message; // Store the error message
-        setIsLoading(false);
+        errorMessageIn = 'Error starting test: ' + error.response.data; 
         setIsModalOpen(false);
+        setIsCompliant(false);
         setErrorMessage(errorMessage); // Display the error message in UI
         return;
       } 
 
     try {
+
+      setIsLoading(true);
 
       const responseStart = await axios.put('http://localhost:3001/test-start'); //returns 200 if picotalker is up and 404 if picotalker not up or Json one key is live, error, timeout, connection refused
       
@@ -77,7 +78,6 @@ const TestConstants = () => {
       setIsModalOpen(false)
 
     } 
-    
   };
 
   const handleOpenModal = () => {
@@ -102,6 +102,15 @@ const TestConstants = () => {
         </div>
         <div className="grid grid-cols-1 gap-4 sm:gap-6 lg:gap-2">
           <div className="border-l border-gray-300 px-14">
+            {!isCompliant && (
+              <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Wrong Inputs!</strong>
+                <span class="block sm:inline">{errorMessage}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                  <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+            )}
             <InputField
               label="Test Name"
               id="testname"
